61de18f6daef9bf81b7fe750fff5d73e3c7833a8
TaylorMaide <dylancopeland89@gmail.com>
2025-02-16 14:00:57 -0500

    Added Break Walls and Reset visited Methods to Maze

--- test.py
+++ test.py
@@ -1,6 +1,7 @@
 import unittest
 
 from maze import Maze
+from cell import Cell
 
 
 class Tests(unittest.TestCase):
@@ -29,6 +30,19 @@ class Tests(unittest.TestCase):
             m1._cells[num_cols - 1][num_rows - 1].has_bottom_wall,
             False,
         )
+    
+    def test_maze_reset_cells_visited(self):
+        num_cols = 3
+        num_rows = 3
+        m1 = Maze(0, 0, num_rows, num_cols, 10, 10)
+        m1._cells[0][0].visited = True
+        m1._cells[1][1].visited = True
+        m1._cells[2][2].visited = True
+        m1._reset_cells_visited()
+
+        for i in range(m1._num_cols):
+            for j in range(m1._num_rows):
+                self.assertFalse(m1._cells[i][j].visited, f"Cell ({i}, {j}) was not reset!")
 
 
 if __name__ == "__main__":
